38a7d062b61d0c514bdff1d9568d5b0299f59bb0
sage <me@laymonage.com>
2026-07-12 17:07:37 +0100

    Fix border

--- src/components/OGImage.tsx
+++ src/components/OGImage.tsx
@@ -15,7 +15,7 @@ export default function OGImage({
   };
   return (
     <div
-      tw="w-full h-full flex flex-col justify-end items-start p-8 bg-[rgb(17,24,39)] bg-position-[-100%_0%] bg-repeat-y border-l-24 border-[rgb(71,85,105)] border-solid text-white"
+      tw="w-full h-full flex flex-col justify-end items-start p-8 bg-[rgb(17,24,39)] bg-position-[-100%_0%] bg-repeat-y border-0 border-l-24 border-[rgb(71,85,105)] border-solid text-white"
       style={style}
     >
       <h1 className="" tw="text-7xl font-semibold tracking-[-0.042em] mb-0">
