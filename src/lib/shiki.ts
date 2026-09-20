fe31cc4308f3ccb09299407c8a27a9f1717aafc2
sage <me@laymonage.com>
2026-09-20 22:39:20 +0100

    Update plugins

--- src/lib/shiki.ts
+++ src/lib/shiki.ts
@@ -1,12 +1,16 @@
 import type { Element } from 'hast';
-import { h } from 'hastscript';
 
 export function copyButton() {
   return {
     name: 'l-copy-button',
     pre(node: Element) {
       node.properties.tabindex = undefined;
-      node.children.push(h('l-copy-button'));
+      node.children.push({
+        type: 'element',
+        tagName: 'l-copy-button',
+        properties: {},
+        children: [],
+      });
     },
   };
 }
